0a3b0b29626c73d01e7c1ca5ff6622ed7a40f379
Joel Pickin <joelpickin@outlook.com>
2022-03-10 15:59:53 +0000

    * Added metric page * Finished milestones page

---
+++ RoadToSolo/Data/Metric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoadToSolo.Data
+{
+    public class Metric
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ RoadToSolo/Pages/Metrics.cs
@@ -0,0 +1,33 @@
+﻿using RoadToSolo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoadToSolo.Pages
+{
+    public partial class Metrics
+    {
+        public List<Metric> MetricsList { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            InitializeMetrics();
+
+            return base.OnInitializedAsync();
+        }
+
+        private void InitializeMetrics()
+        {
+            MetricsList = new List<Metric>()
+            {
+                new Metric { Title = "Days Runway Remaining", Value="196" },
+                new Metric { Title = "Days Until I Leave", Value="204" },
+                new Metric { Title = "Overall Revenue", Value="£0" },
+                new Metric { Title = "MRR", Value="£0" },
+                new Metric { Title = "ARR", Value="£0" },
+                new Metric { Title = "Projects Created", Value="1" }
+            };
+        }
+    }
+}
