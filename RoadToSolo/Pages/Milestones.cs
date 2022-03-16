7e7a8a62ff48f3964beafc0edaa66f1de7f93d8a
Joel Pickin <joelpickin@outlook.com>
2022-03-16 11:11:19 +0000

    - Added Product Hunt logo and link - Changed milestones to use emojis - Changed milestone achieved styling - Take in PostId in posts page - Changed a lot of stylings

--- RoadToSolo/Pages/Milestones.cs
+++ RoadToSolo/Pages/Milestones.cs
@@ -22,15 +22,15 @@ namespace RoadToSolo.Pages
             MilestonesList = new List<Milestone>()
             {
                 new Milestone{ Title="Release Website", IsAchieved=true, AchievedDate="07 Mar 2022" },
-                new Milestone{ Title="Release Project 1", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="First dollar made!", MilestoneIndicator="$1", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Release Project 2", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Release Project 3", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Release Project 4", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Web 3 Project", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Video Game Project", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Self-taught Developers Project", PredictedDate="07 Mar 2022" },
-                new Milestone{ Title="Amazon Alexa Skill", PredictedDate="07 Mar 2022" },
+                new Milestone{ Title="Release Project 1", MilestoneIndicator="1️⃣" },
+                new Milestone{ Title="Release Project 2", MilestoneIndicator="2️⃣" },
+                new Milestone{ Title="Release Project 3", MilestoneIndicator="3️⃣" },
+                new Milestone{ Title="Release Project 4", MilestoneIndicator="4️⃣" },
+                new Milestone{ Title="First dollar made", MilestoneIndicator="💰" },
+                new Milestone{ Title="Web 3 Project", MilestoneIndicator="🚀" },
+                new Milestone{ Title="Video Game Project", MilestoneIndicator="🎮" },
+                new Milestone{ Title="Community Project", MilestoneIndicator="👥" },
+                new Milestone{ Title="Amazon Alexa Skill", MilestoneIndicator="🗣️" },
             };
         }
     }
--- RoadToSolo/Pages/Phases.cs
+++ RoadToSolo/Pages/Phases.cs
@@ -1,17 +1,12 @@
-﻿using Microsoft.AspNetCore.Components;
-using RoadToSolo.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoadToSolo.Pages
 {
-    public partial class ViewProject
+    public partial class Phases
     {
-        [Parameter]
-        public string ProjectId { get; set; }
-        public Project Project { get; set; }
         protected override Task OnInitializedAsync()
         {
             return base.OnInitializedAsync();
