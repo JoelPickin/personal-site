90c6b5d0fd50e42cf59cf509b9715ffb8fc184da
Joel Pickin <joelpickin@outlook.com>
2022-03-22 17:55:47 +0000

    * Check for environment in setup

--- RoadToSolo/Startup.cs
+++ RoadToSolo/Startup.cs
@@ -62,9 +62,9 @@ namespace RoadToSolo
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
