ead556503dc2a5e3bb9696b79a128fd75a114f8d
Joel Pickin <joelpickin@outlook.com>
2022-03-23 07:38:56 +0000

    - Removed test post - Added no posts text - Increase subscribe form padding - Removed commented HTTPS redirect code

--- RoadToSolo/Data/Repositories/PostRepository.cs
+++ RoadToSolo/Data/Repositories/PostRepository.cs
@@ -10,7 +10,7 @@ namespace RoadToSolo.Data.Repositories
     {
         private List<Post> _posts = new List<Post>()
         {
-            new Post{Id=1, Title="Test Post 1", SubHeading="This is a test post 1", Content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec risus erat, efficitur a accumsan id, facilisis at sapien. Sed scelerisque urna et massa blandit blandit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed non magna ullamcorper, rhoncus augue nec, iaculis lectus. Maecenas ultrices dictum mauris, non feugiat erat ullamcorper at. Etiam vel elementum orci, ac sodales lacus. Sed aliquet nisl nec felis scelerisque, in hendrerit neque interdum."}
+            
         };
 
         public List<Post> GetAll()
